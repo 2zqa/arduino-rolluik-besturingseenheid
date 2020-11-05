0304dc335c81f8feaebed0a5f570ea987bead4d5
2zqa <25235249+2zqa@users.noreply.github.com>
2020-11-05 11:31:07 +0100

    Gebruik scheduler

--- arduino-rolluik-besturingseenheid/main.c
+++ arduino-rolluik-besturingseenheid/main.c
@@ -1,36 +1,237 @@
+#include "main.h"
+
+// Code voor serial
+#include "mijn_serial.h"
 #include <avr/io.h>
-#include <stdlib.h>
-#include <avr/sfr_defs.h>
-#define F_CPU 16E6
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
-// output on USB = PD1 = board pin 1
-// datasheet p.190; F_OSC = 16 MHz & baud rate = 19.200
-#define UBBRVAL 51
+// The array of tasks
+sTask SCH_tasks_G[SCH_MAX_TASKS];
 
-void uart_init() {
-    // set the baud rate
-    UBRR0H = 0;
-    UBRR0L = UBBRVAL;
-    // disable U2X mode
-    UCSR0A = 0;
-    // enable transmitter and receiver
-    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
-    // set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+// index van status task
+uint8_t status_index = 0;
+
+
+/*------------------------------------------------------------------*-
+
+  SCH_Dispatch_Tasks()
+
+  This is the 'dispatcher' function.  When a task (function)
+  is due to run, SCH_Dispatch_Tasks() will run it.
+  This function must be called (repeatedly) from the main loop.
+
+-*------------------------------------------------------------------*/
+
+void SCH_Dispatch_Tasks(void)
+{
+   unsigned char Index;
+
+   // Dispatches (runs) the next task (if one is ready)
+   for(Index = 0; Index < SCH_MAX_TASKS; Index++)
+   {
+      if((SCH_tasks_G[Index].RunMe > 0) && (SCH_tasks_G[Index].pTask != 0))
+      {
+         (*SCH_tasks_G[Index].pTask)();  // Run the task
+         SCH_tasks_G[Index].RunMe -= 1;   // Reset / reduce RunMe flag
+
+         // Periodic tasks will automatically run again
+         // - if this is a 'one shot' task, remove it from the array
+         if(SCH_tasks_G[Index].Period == 0)
+         {
+            SCH_Delete_Task(Index);
+         }
+      }
+   }
 }
 
-void transmit(uint8_t data) {
-    // wait for an empty transmit buffer
-    // UDRE is set when the transmit buffer is empty
-    loop_until_bit_is_set(UCSR0A,UDRE0);
-    // send the data
-    UDR0 = data;
+/*------------------------------------------------------------------*-
+
+  SCH_Add_Task()
+
+  Causes a task (function) to be executed at regular intervals 
+  or after a user-defined delay
+
+  pFunction - The name of the function which is to be scheduled.
+              NOTE: All scheduled functions must be 'void, void' -
+              that is, they must take no parameters, and have 
+              a void return type. 
+                   
+  DELAY     - The interval (TICKS) before the task is first executed
+
+  PERIOD    - If 'PERIOD' is 0, the function is only called once,
+              at the time determined by 'DELAY'.  If PERIOD is non-zero,
+              then the function is called repeatedly at an interval
+              determined by the value of PERIOD (see below for examples
+              which should help clarify this).
+
+
+  RETURN VALUE:  
+
+  Returns the position in the task array at which the task has been 
+  added.  If the return value is SCH_MAX_TASKS then the task could 
+  not be added to the array (there was insufficient space).  If the
+  return value is < SCH_MAX_TASKS, then the task was added 
+  successfully.  
+
+  Note: this return value may be required, if a task is
+  to be subsequently deleted - see SCH_Delete_Task().
+
+  EXAMPLES:
+
+  Task_ID = SCH_Add_Task(Do_X,1000,0);
+  Causes the function Do_X() to be executed once after 1000 sch ticks.            
+
+  Task_ID = SCH_Add_Task(Do_X,0,1000);
+  Causes the function Do_X() to be executed regularly, every 1000 sch ticks.            
+
+  Task_ID = SCH_Add_Task(Do_X,300,1000);
+  Causes the function Do_X() to be executed regularly, every 1000 ticks.
+  Task will be first executed at T = 300 ticks, then 1300, 2300, etc.            
+ 
+-*------------------------------------------------------------------*/
+
+unsigned char SCH_Add_Task(void (*pFunction)(), const unsigned int DELAY, const unsigned int PERIOD)
+{
+   unsigned char Index = 0;
+
+   // First find a gap in the array (if there is one)
+   while((SCH_tasks_G[Index].pTask != 0) && (Index < SCH_MAX_TASKS))
+   {
+      Index++;
+   }
+
+   // Have we reached the end of the list?   
+   if(Index == SCH_MAX_TASKS)
+   {
+      // Task list is full, return an error code
+      return SCH_MAX_TASKS;  
+   }
+
+   // If we're here, there is a space in the task array
+   SCH_tasks_G[Index].pTask = pFunction;
+   SCH_tasks_G[Index].Delay =DELAY;
+   SCH_tasks_G[Index].Period = PERIOD;
+   SCH_tasks_G[Index].RunMe = 0;
+
+   // return position of task (to allow later deletion)
+   return Index;
 }
 
-uint8_t receive() {
-    loop_until_bit_is_set(UCSR0A,RXC0); // dit kan een tijdje duren, of zelfs oneindig!
-    return UDR0;
+/*------------------------------------------------------------------*-
+
+  SCH_Delete_Task()
+
+  Removes a task from the scheduler.  Note that this does
+  *not* delete the associated function from memory: 
+  it simply means that it is no longer called by the scheduler. 
+ 
+  TASK_INDEX - The task index.  Provided by SCH_Add_Task(). 
+
+  RETURN VALUE:  RETURN_ERROR or RETURN_NORMAL
+
+-*------------------------------------------------------------------*/
+
+unsigned char SCH_Delete_Task(const unsigned char TASK_INDEX)
+{
+   // Return_code can be used for error reporting, NOT USED HERE THOUGH!
+   unsigned char Return_code = 0;
+
+   SCH_tasks_G[TASK_INDEX].pTask = 0;
+   SCH_tasks_G[TASK_INDEX].Delay = 0;
+   SCH_tasks_G[TASK_INDEX].Period = 0;
+   SCH_tasks_G[TASK_INDEX].RunMe = 0;
+
+   return Return_code;
+}
+
+/*------------------------------------------------------------------*-
+
+  SCH_Init_T1()
+
+  Scheduler initialisation function.  Prepares scheduler
+  data structures and sets up timer interrupts at required rate.
+  You must call this function before using the scheduler.  
+
+-*------------------------------------------------------------------*/
+
+void SCH_Init_T1(void)
+{
+   unsigned char i;
+
+   for(i = 0; i < SCH_MAX_TASKS; i++)
+   {
+      SCH_Delete_Task(i);
+   }
+
+   // Set up Timer 1
+   // Values for 1ms and 10ms ticks are provided for various crystals
+
+   // Hier moet de timer periode worden aangepast ....!
+   OCR1A = (uint16_t)625;   		     // 10ms = (256/16.000.000) * 625
+   TCCR1B = (1 << CS12) | (1 << WGM12);  // prescale op 64, top counter = value OCR1A (CTC mode)
+   TIMSK1 = 1 << OCIE1A;   		     // Timer 1 Output Compare A Match Interrupt Enable
+}
+
+/*------------------------------------------------------------------*-
+
+  SCH_Start()
+
+  Starts the scheduler, by enabling interrupts.
+
+  NOTE: Usually called after all regular tasks are added,
+  to keep the tasks synchronised.
+
+  NOTE: ONLY THE SCHEDULER INTERRUPT SHOULD BE ENABLED!!! 
+ 
+-*------------------------------------------------------------------*/
+
+void SCH_Start(void)
+{
+      sei();
+}
+
+/*------------------------------------------------------------------*-
+
+  SCH_Update
+
+  This is the scheduler ISR.  It is called at a rate 
+  determined by the timer settings in SCH_Init_T1().
+
+-*------------------------------------------------------------------*/
+
+ISR(TIMER1_COMPA_vect)
+{
+   unsigned char Index;
+   for(Index = 0; Index < SCH_MAX_TASKS; Index++)
+   {
+      // Check if there is a task at this location
+      if(SCH_tasks_G[Index].pTask)
+      {
+         if(SCH_tasks_G[Index].Delay == 0)
+         {
+            // The task is due to run, Inc. the 'RunMe' flag
+            SCH_tasks_G[Index].RunMe += 1;
+
+            if(SCH_tasks_G[Index].Period)
+            {
+               // Schedule periodic tasks to run again
+               SCH_tasks_G[Index].Delay = SCH_tasks_G[Index].Period;
+               SCH_tasks_G[Index].Delay -= 1;
+            }
+         }
+         else
+         {
+            // Not yet ready to run: just decrement the delay
+            SCH_tasks_G[Index].Delay -= 1;
+         }
+      }
+   }
+}
+
+// ------------------------------------------------------------------
+
+void init_rolluik() {
+	DDRB = 0xff;
 }
 
 /*
@@ -42,34 +243,33 @@ void oprollen() {
 
 void uitrollen() {
     PORTB = 0x02;
+	status_index = SCH_Add_Task(status,0,10);
 }
 
+/*
+	Schakel gele lampje
+*/
 void status() {
-    PORTB = 0x04;
+    PORTB ^= 0x04;
+	transmit(status_index);
 }
 
-int main(void) {
-    uart_init();
-    DDRB = 0xff; // stel op poort D alles in op "output"
-    PORTB = 0xff; // zet lampjes aan aan
-    while(1) {
-        uint8_t received_bit = receive();
-        //transmit(received_bit); // stuur terug wat je ontvangt, handig voor debugging
-        
-        // tijdelijke info:
-        // 0x01 betekent oprollen
-        // 0x02 betekent uitrollen
-        // 
-        switch (received_bit) {
-            case 0x01:
-                oprollen();
-                break;
-            case 0x02:
-                uitrollen();
-                break;
-			case 0x03:
-				status();
-				break;
-        }
-    }
+void stop_status() {
+	SCH_Delete_Task(status_index);
+}
+
+int main()
+{
+	SCH_Init_T1(); // init de timeren verwijder alle taken
+	SCH_Start();   // start de scheduler
+	
+	// Eenmalige functies
+	init_rolluik();
+	uart_init();
+	SCH_Add_Task(uitrollen,1000,0);
+	
+	while (1) {
+		SCH_Dispatch_Tasks();
+	}
+	return 0;
 }
